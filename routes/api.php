a62a570efc16b002b91b1d0285334b87c1e5ae84
jefri tri <jefritrimujipangestu09@gmail.com>
2020-08-31 16:40:13 +0700

    chalenge

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,5 @@ Route::post('/books/create', 'BookController@store');
 Route::post('/books/update/{id}', 'BookController@update');
 
 Route::delete('/books/delete/{id}', 'BookController@destroy');
+//AuthorChallange
+Route::resource('authors', 'AuthorController');
